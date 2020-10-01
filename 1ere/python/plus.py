811c61824fecfa932349b52dee800614d50d8943
QuentinBubu <anagamesprint@gmail.com>
2020-10-01 19:08:03 +0200

    Update: add functions

--- 1ere/python/plus.py
+++ 1ere/python/plus.py
@@ -16,15 +16,17 @@ def sphinx_aime(nombre):
         return "Il aime le chiffre/nombre"
     else:
         return "Il aime pas le chiffre/nombre!"
-        
-        
 
 def code_hall():
+    if nombre > 0:
+        return "Pas strictement positif"
     for i in range(2,100):
         if est_premier(i+2):
             return i
 
 def est_puissance_2(nb):
+    if nombre > 0:
+        return "Pas strictement positif"
     a = math.sqrt(nb)
     b = str(a).split(".")
     if  nb == a**2 and b[1] == "0":
@@ -33,5 +35,19 @@ def est_puissance_2(nb):
         return False
         
 def osiris_aime(nombre):
+    if nombre > 0:
+        return "Pas strictement positif"
     if est_puissance_2(nombre+1) and nombre%5 == 3:
-        
+        return True
+    else:
+        return False
+    
+def osiris_aime2():
+    for i in range(1, 100):
+        if est_puissance_2(i+1) and i%5 == 3:
+            return i
+        else:
+            return False
+    
+def code_tresor():
+    pass
