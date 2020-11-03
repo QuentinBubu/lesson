1a90bc52700091114c12dc5cb52c45c8bdabeec6
QuentinBubu <anagamesprint@gmail.com>
2020-11-03 22:35:07 +0100

    Create td2_representation_des_donnees.py

---
+++ 1ere/python/td2_representation_des_donnees.py
@@ -0,0 +1,111 @@
+# Ex 1
+"""
+listInf = [i for i in range(0,1000)]
+print(listInf)
+
+listCarres = [i**2 for i in listInf]
+# Or
+listCarres = [i**2 for i in range(0,1000)]
+print(listCarres)
+
+listBaseCarres = [i for i in listCarres if i < 1000]
+print(listBaseCarres)
+
+listCubes = [i**3 for i in range(0,1000)]
+print(listCubes)
+
+listBaseCubes = [i for i in listCubes if i < 1000]
+print(listBaseCubes)
+
+listCarresCubes = [i for i in listBaseCubes if i in listBaseCarres]
+print(listCarresCubes)
+
+
+# Ex 2
+
+def calcSomme(number):
+    number = str(number)
+    somme = 0
+    for i in range(len(number)):
+        somme += int(number[i])
+    return somme
+
+def calcExp3(number):
+    return number**3
+
+def rechercheDudeney():
+    listValid = []
+    for i in range(1,100000):
+        if calcExp3(calcSomme(i)) == i:
+            listValid.append(i)
+    return listValid
+
+print(rechercheDudeney())
+
+"""
+
+# Ex 2+
+
+def calcSomme(number):
+    number = list(str(number))
+    somme = 0
+    for i in range(len(number)):
+        somme += int(number[i])
+    return somme
+
+# !
+def calcSommeDix(number):
+    result = 0
+    while number != 0:
+        result += number%10
+        number -= number%10
+        #number /= 10
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+def calcExp3(number):
+    return number**3
+
+def rechercheDudeney():
+    listValid = []
+    for i in range(1,100000):
+        if calcExp3(calcSomme(i)) == i:
+            listValid.append(i)
+    return listValid
+
+print(rechercheDudeney())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
