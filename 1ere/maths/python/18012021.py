37d83324b49056eea3c5106ee3fcb43ede0671a4
QuentinBubu <anagamesprint@gmail.com>
2021-01-18 21:25:31 +0100

    Commit Maths file 18012021

---
+++ 1ere/maths/python/18012021.py
@@ -0,0 +1,11 @@
+# Activité 1
+
+# 1
+
+"""
+l1 => Affect le nombre 64 à n
+l2 => Affect le reste de la division de n par 10 à u soit 4
+l3 => Affect le quotient entier de la division de n par 10 à d soit 6
+l4 => Effectue l'opération: 10*u + d soit 10*4 + 6 et affecte le résultat à m
+l5 => Affiche la variable m soit 46
+"""
